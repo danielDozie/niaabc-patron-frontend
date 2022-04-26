dd312720515330043bc4cf5ed7aa6c6766fcab78
badt0men <placmedia@gmail.com>
2022-04-26 16:28:31 +0100

    color-mode-

--- src/components/pages/architects/MemberCard.tsx
+++ src/components/pages/architects/MemberCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const MemberCard = () => {
   return (
-    <div className="w-[150px] h-[230px] md:h-[200px] md:w-[200px] rounded-md  mx-auto shadow-sm bg-gradient-to-tl from-white to-slate-50 || dark:bg-gradient-to-tl dark:from-gray-900 dark:to-slate-900">
+    <div className="w-[160px] h-[230px] md:h-[200px] md:w-[200px] rounded-md  mx-auto shadow-sm bg-gradient-to-tl from-white to-slate-50 || dark:bg-gradient-to-tl dark:from-gray-900 dark:to-slate-900">
                     <img src="../assets/image/profile-img.jpeg" className="w-20 h-20 rounded-full mt-4 mx-auto" />
                     <div className="px-4 text-slate-600 mt-4 text-center md:text-left font-light || dark:text-slate-400">
                         <p className="text-xs">
